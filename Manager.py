3a7222ce3b7764b28fdd1e79027357414d906853
netanelye <netanelye@mta.ac.il>
2021-02-06 12:54:20 +0200

    finishing_1

--- Manager.py
+++
@@ -1,5 +0,0 @@
-class Manager:
-    def __init__(self):
-        self.__schedule = []
-        self.__matrix = []
-        self.__colors = []
--- main.py
+++ main.py
@@ -12,11 +12,7 @@ def createList(listOfScheduleAsString):
 
 def main():
     scheduleAsString = input("Please enter a schedule\n")
-    # scheduleAsString = "r2(A);r1(B);w2(A);r2(B);r3(A);w1(B);w3(A);w2(B)"
-    # scheduleAsString = "R2(A);R1(B);W2(A);R2(B);R3(A);W1(B);W3(A);W2(B)"
-    # scheduleAsString = "R2(A);R1(B);W2(A);R3(A);W1(B);W3(A);R2(B);W2(B)"
     listOfSchedule = createList(scheduleAsString.split(";"))
-    # print(listOfSchedule)
     graph = Graph(listOfSchedule)
     graph.topological_sort()
     graph.get_topological_sort()
